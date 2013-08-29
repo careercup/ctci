5b775cd173ea60d06e9520a29f2004cdd2a5424c
charnugagoo <charnugagoo@gmail.com>
2013-08-29 15:12:55 -0400

    Simplify list index

--- python/Chapter 1/Question1_2/ChapQ1.2.py
+++ python/Chapter 1/Question1_2/ChapQ1.2.py
@@ -4,7 +4,7 @@
 
 def reverseStringRecursive(str1):
     if str1 != "":
-        return str1[len(str1)-1:len(str1)] + reverseStringRecursive(str1[0:len(str1)-1])
+        return str1[-1:] + reverseStringRecursive(str1[:-1])
     else:
         return ""
 
