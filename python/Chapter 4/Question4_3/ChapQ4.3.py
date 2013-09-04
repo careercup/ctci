59536ab49ae6854842372f7f7f418acf9004185b
charnugagoo <charnugagoo@gmail.com>
2013-09-03 23:49:35 -0400

    Simplify code

--- python/Chapter 4/Question4_3/ChapQ4.3.py
+++ python/Chapter 4/Question4_3/ChapQ4.3.py
@@ -1,9 +1,9 @@
 #binary tree python
 class BinaryTree:
-    def __init__(self, content):
+    def __init__(self, content, left = None, right = None):
         self.content = content
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
 
     def __str__(self):
         return "( " + str(self.content) + " ( " + str(self.left) + " | " + str(self.right) + "))" 
@@ -14,15 +14,14 @@ class BinaryTree:
 
 def int_array_to_binary_search_tree(intarray):
     #use the middle of the array to divide it. this ensures minimal height.
-    if len(intarray)==0:
+    if len(intarray) == 0:
         return None
-    if len(intarray)==1:
+    if len(intarray) == 1:
         return BinaryTree(intarray[0])
-    cut=int(len(intarray)/2)
-    return_bt = BinaryTree(intarray[cut])
-    return_bt.left=int_array_to_binary_search_tree(intarray[0:cut])
-    return_bt.right=int_array_to_binary_search_tree(intarray[cut+1:len(intarray)])
-    return return_bt
+    cut = len(intarray) / 2
+    return BinaryTree( intarray[cut], \
+        int_array_to_binary_search_tree(intarray[0:cut]), 
+        int_array_to_binary_search_tree(intarray[cut+1:]))
 
 #testing
 
