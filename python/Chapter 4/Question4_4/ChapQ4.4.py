3bdf19c9ed196377f4dafe1ae53c9bd964777457
charnugagoo <charnugagoo@gmail.com>
2013-09-04 18:17:35 -0400

    ChapQ4.4
    plus sample

--- python/Chapter 4/Question4_4/ChapQ4.4.py
+++ python/Chapter 4/Question4_4/ChapQ4.4.py
@@ -76,9 +76,9 @@ for ll in binary_tree_to_list_of_linked_lists(balanced_tree):
     count += 1
     print str(count) + ": " + str(ll)
 
-print "list version", binary_tree_to_list(balanced_tree)
+print "\nlist version", binary_tree_to_list(balanced_tree)
     
-#sample output
+#sample output of binary_tree_to_list_of_linked_lists
 ##( 60 ( ( 59 ( ( 32 ( ( 95 ( ( 42 ( None | None)) | ( 11 ( None | None)))) | ( 1 ( ( 56 ( None | None)) | ( 11 ( None | None)))))) | ( 26 ( ( 25 ( ( 68 ( None | None)) | ( 56 ( None | None)))) | ( 29 ( ( 41 ( None | None)) | ( 21 ( None | None)))))))) | ( 88 ( ( 63 ( ( 58 ( ( 4 ( None | None)) | ( 70 ( None | None)))) | ( 30 ( ( 53 ( None | None)) | ( 38 ( None | None)))))) | ( 4 ( ( 93 ( ( 91 ( None | None)) | ( 21 ( None | None)))) | ( 55 ( ( 62 ( None | None)) | ( 25 ( None | None))))))))))
 ##
 ##Is changed to:
@@ -88,3 +88,6 @@ print "list version", binary_tree_to_list(balanced_tree)
 ##3: ( 4( 63( 26( 32None ) ) ) )
 ##4: ( 55( 93( 30( 58( 29( 25( 1( 95None ) ) ) ) ) ) ) )
 ##5: ( 25( 62( 21( 91( 38( 53( 70( 4( 21( 41( 56( 68( 11( 56( 11( 42None ) ) ) ) ) ) ) ) ) ) ) ) ) ) ) )
+
+#sample output of binary_tree_to_list_of_linked_lists
+#list version [[18], [48, 46], [12, 80, 100, 45], [23, 9, 85, 73, 52, 40, 65, 30], [43, 94, 53, 94, 96, 35, 30, 37, 28, 9, 95, 84, 74, 49, 45, 24]]
