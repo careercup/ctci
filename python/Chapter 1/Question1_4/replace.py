73bce3b2dc9054f95e9383ffd63ebce358f8273a
Michele Filannino <filannino.m@gmail.com>
2014-06-09 12:59:51 +0100

    Using str.replace()

--- python/Chapter 1/Question1_4/replace.py
+++ python/Chapter 1/Question1_4/replace.py
@@ -1,6 +1,9 @@
 def replaceSpaces(s):
     return '%20'.join(s.split())
 
+def replace_spaces(string, size):
+    return string[:size].replace(' ', '%20')
+
 if __name__ == '__main__':
     words = ( 'hello world    ', '   mr. john  ', '', ' ', )
     for w in words:
