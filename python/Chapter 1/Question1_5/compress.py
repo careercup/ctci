b4a4a5b0e3cce441c62a243a9929ece2ac209f82
Víctor Terrón <vterron@iaa.es>
2014-07-18 11:50:04 +0200

    Add another implementation of compress() — using itertools.groupby()

--- python/Chapter 1/Question1_5/compress.py
+++ python/Chapter 1/Question1_5/compress.py
@@ -11,6 +11,28 @@ def compress(string):
             counter = 1
     return result + c + str(counter)
 
+import itertools
+
+def compress(str_):
+    """ Using itertools.groupby().
+
+    There is a function in the standard library, itertools.groupby() (new in
+    Python 2.4), that does exactly what we need here: it makes an iterator that
+    returns consecutive keys and groups from the input iterable, defaulting to
+    an identity function to compute the key value for each element. Example:
+
+    >>> [list(g) for k, g in itertools.groupby('AAAABBBCCD')]
+    [['A', 'A', 'A', 'A'], ['B', 'B', 'B'], ['C', 'C'], ['D']]
+
+    """
+
+    groups = itertools.groupby(str_)
+    compressed = ''.join('{0}{1}'.format(k, len(list(g))) for k, g in groups)
+    if len(compressed) < len(str_):
+        return compressed
+    else:
+        return str_
+
 if __name__ == '__main__':
     words = ('aabcccccaaa', 'aaabbbbcccccdee', 'abc', 'a', '', ' ', 'aabccdde', 'aab', 'aaabb')
     for w in words:
