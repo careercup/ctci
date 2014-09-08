9a67d56f6ec45e2f97d7d340a65326e9aac0a1b6
kristianjaeger <kristianjaeger@gmail.com>
2014-09-08 16:49:55 -0700

    Added missing questions chapter 7 - C-Sharp
    Added missing questions chapter 7 - C-Sharp

---
+++ c-sharp/Chapter07/Q07_7.cs
@@ -0,0 +1,66 @@
+﻿
+using ctci.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace Chapter07
+{
+    public class Q07_7 : IQuestion
+    {
+        // Note: using List instead of Queue since .NET Queue does not have remove
+        public static int RemoveMin(List<Int32> list)
+        {
+            var index = 0;
+            var min = list[0];
+
+		    for (var loopCounter = 0; loopCounter < list.Count; loopCounter++)
+            {
+                if (min > list[loopCounter])
+                {
+                    min = list[loopCounter];
+                    index = loopCounter;
+                }
+		    }
+
+            list.Remove(index);
+           
+		    return min;
+	    }
+
+        public static void AddProducts(List<Int32> q, int v)
+        {
+            q.Add(v * 3);
+            q.Add(v * 5);
+            q.Add(v * 7);
+        }
+
+        public static int GetKthMagicNumber(int k)
+        {
+            if (k < 0)
+            {
+                return 0;
+            }
+
+            var val = 1;
+            var q = new List<Int32>();
+            AddProducts(q, 1);
+
+            for (var i = 0; i < k; i++)
+            { 
+                // Start at 1 since we've already done one iteration
+                val = RemoveMin(q);
+                AddProducts(q, val);
+            }
+            
+            return val;
+        }
+
+        public void Run()
+        {
+		    for (var i = 0; i < 14; i++) 
+            {
+			    Console.WriteLine(i + " : " + GetKthMagicNumber(i));
+		    }
+        }
+    }
+}
--- c-sharp/ctci/Program.cs
+++ c-sharp/ctci/Program.cs
@@ -1,6 +1,5 @@
-using System;
+
 using ctci.Contracts;
-using Introduction;
 using Chapter01;
 using Chapter02;
 using Chapter03;
@@ -11,6 +10,8 @@ using Chapter09;
 using Chapter11;
 using Chapter17;
 using Chapter18;
+using Introduction;
+using System;
 
 namespace ctci
 {
@@ -18,7 +19,7 @@ namespace ctci
     {
         static void Main(string[] args)
         {
-            IQuestion[] questions = new IQuestion[]
+            IQuestion[] questions = 
             {
                 // Intro
                 new CompareBinaryToHex(), new SwapMinMax(), 
@@ -29,7 +30,7 @@ namespace ctci
                 new Q03_1_A(), new Q03_1_B(),   new Q03_2(), new Q03_3(), new Q03_4(), new Q03_5(), new Q03_6(), new Q03_7(),
                 new Q04_1(),                    new Q04_2(), new Q04_3(), new Q04_4(), new Q04_5(), new Q04_6(), new Q04_7(), new Q04_8(), new Q04_9(),
                                                 new Q05_1(), new Q05_2(), new Q05_3(), new Q05_5(), new Q05_6(), new Q05_7(), new Q05_8(), 
-                                                                                                    new Q07_6(),
+                                                                          new Q07_3(), new Q07_4(), new Q07_5(), new Q07_6(), new Q07_7(), 
                                                                                                     new Q09_6(),
                 new Q11_1(),                    new Q11_2(),
                                                              new Q17_3(), new Q17_4(),                                                                                new Q17_11(), new Q17_12(),
