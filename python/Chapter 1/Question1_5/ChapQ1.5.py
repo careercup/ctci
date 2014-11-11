8c76dccbc091536abd2aa01b7b2808fc7642ee7d
caferyil <cafertyildirim@gmail.com>
2014-11-11 15:47:56 -0500

    Update ChapQ1.5.py

--- python/Chapter 1/Question1_5/ChapQ1.5.py
+++ python/Chapter 1/Question1_5/ChapQ1.5.py
@@ -42,3 +42,28 @@ if simpleCompress(tocompress2) == tocompress2:
 else:
     print "Test 2 Failed"
 
+
+#Modulated version of the solution
+def countSame(string, pos):
+	#Counts number of same characters following a position
+	pos1 = pos
+	count = 0
+	while pos1 < len(string) and string[pos1] == string[pos]:
+		count = count +1
+		pos1 = pos1 + 1
+	return count
+
+
+def compress(string):
+	outstring = []
+	pos = 0
+	while pos < len(string):
+		outstring.append(string[pos] + str(countSame(string, pos)))
+		pos = pos + countSame(string, pos)
+	result = ''.join(outstring)
+	if len(result) < len(string):
+	    return result
+	else:
+	    retunr string
+
+print compress("aabccc   ccaaa")
