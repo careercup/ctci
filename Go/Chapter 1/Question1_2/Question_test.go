2a619fbfc3c9070d0c3ba342885c636acd57bbd8
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2014-12-20 21:58:41 +0100

    added 1_2

---
+++ Go/Chapter 1/Question1_2/Question_test.go
@@ -0,0 +1,22 @@
+package Question1_2
+
+import (
+	"testing"
+)
+
+func TestIsUniqueChars(t *testing.T) {
+	cases := []struct {
+		input  string
+		expect string
+	}{
+		{"abcd", "dcba"},
+		{"abcde", "edcba"},
+	}
+
+	for _, thisCase := range cases {
+		result := reverse(thisCase.input)
+		if result != thisCase.expect {
+			t.Errorf("reverse: input %q, expect %q, but got %q\n", thisCase.input, thisCase.expect, result)
+		}
+	}
+}
