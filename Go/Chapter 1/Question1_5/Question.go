29e32fcac18ce7e3640edd91a984699ee757ad58
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2014-12-23 15:35:20 +0100

    Question1_5 good to go

---
+++ Go/Chapter 1/Question1_5/Question.go
@@ -0,0 +1,37 @@
+package Question1_5
+
+import "bytes"
+import "fmt"
+
+// Helper function that counts number of same characters following a position
+func countSame(runeStr []rune, pos int) int {
+	posCheck := pos + 1
+	count := 1
+	for posCheck < len(runeStr) && runeStr[posCheck] == runeStr[pos] {
+		count++
+		posCheck++
+	}
+	return count
+}
+
+// method to perform basic string compression
+// using the counts of repeated characters
+// example "aabcccccaaa" => "a2b1c5a3"
+// But, if the compressed string is not smaller return the original
+func compress(str string) string {
+	//  most efficient way to concat a string in go - http://goo.gl/hIj38X
+	var buffer bytes.Buffer
+	originalStr := []rune(str)
+	for i := 0; i < len(str); {
+		buffer.WriteString(string(originalStr[i]))
+		countSame := countSame(originalStr, i)
+		fmt.Println(countSame)
+		buffer.WriteString(fmt.Sprintf("%d", countSame))
+		i = i + countSame
+	}
+	stringReturn := buffer.String()
+	if len(stringReturn) > len(str) {
+		return str
+	}
+	return stringReturn
+}
