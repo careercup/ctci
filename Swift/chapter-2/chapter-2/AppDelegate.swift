b2f08687d40114a98a1d5e0a54b3e53dc07e765a
prakashn27 <nprakashece@gmail.com>
2014-12-23 22:54:20 -0500

    added Question 5.1

---
+++ Swift/chapter-2/chapter-2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  chapter-2
+//
+//  Created by Prakash on 12/22/14.
+//  Copyright (c) 2014 prakashn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swift/chapter-2/chapter-2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  chapter-2
+//
+//  Created by Prakash on 12/22/14.
+//  Copyright (c) 2014 prakashn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
