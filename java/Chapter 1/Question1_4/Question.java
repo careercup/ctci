c6b465495e37f1e65a235914fc63e9859e44021c
manycoding <manycoding@users.noreply.github.com>
2015-03-03 18:21:21 -0800

    Added exit from a loop

--- java/Chapter 1/Question1_4/Question.java
+++ java/Chapter 1/Question1_4/Question.java
@@ -1,38 +1,36 @@
 package Question1_4;
 
-import CtCILibrary.AssortedMethods;
-
 public class Question {
 	// Assume string has sufficient free space at the end
-	public static void replaceSpaces(char[] str, int length) {
-		int spaceCount = 0, index, i = 0;
-		for (i = 0; i < length; i++) {
+	public static char[] replaceSpaces(char[] str, int length) {
+		int spaceCount = 0;
+		int index = 0;
+
+		for (int i = 0; i < length; i++) {
 			if (str[i] == ' ') {
 				spaceCount++;
 			}
 		}
 		index = length + spaceCount * 2;
 		str[index] = '\0';
-		for (i = length - 1; i >= 0; i--) {
+		for (int i = length - 1; i >= 0; i--) {
 			if (str[i] == ' ') {
 				str[index - 1] = '0';
 				str[index - 2] = '2';
 				str[index - 3] = '%';
 				index = index - 3;
+				
+				if (--spaceCount == 0)
+					break;
 			} else {
 				str[index - 1] = str[i];
 				index--;
 			}
 		}
+		return str;
 	}
 	
 	public static void main(String[] args) {
-		String str = "abc d e f";
-		char[] arr = new char[str.length() + 3 * 2 + 1];
-		for (int i = 0; i < str.length(); i++) {
-			arr[i] = str.charAt(i);
-		}
-		replaceSpaces(arr, str.length());	
-		System.out.println("\"" + AssortedMethods.charArrayToString(arr) + "\"");
+		System.out.println(replaceSpaces("foo bar$ ".toCharArray(), 8));
 	}
 }
