28705ba2ccc6bcd9bcfb20831a547bfa416d6024
santhosh <santhoshvai@gmail.com>
2015-03-20 12:54:44 +0100

    Adding helper functions

--- Go/Chapter 3/stack/stack.go
+++ Go/Chapter 3/stack/stack.go
@@ -1,6 +1,12 @@
 package stack // simple LIFO stack
 
-import "errors"
+import (
+	"errors"
+	"math/rand"
+	"bytes"
+	"fmt"
+	"time"
+)
 
 var errorEmptyStack = errors.New("error: stack is empty")
 
@@ -35,4 +41,42 @@ func (s *Stack) Pop() (int, error) {
 		return val, nil
 	}
 	return 0, errorEmptyStack
+}
+
+func (s *Stack) Peek() (int, error) {
+	if s.size > 0 {
+		return s.top.value, nil
+	}
+	return 0, errorEmptyStack
+}
+/* Helper function to get a random integer (min, max)*/
+func RandIntBetween(min int, max int) int {
+    return min + rand.Intn(max-min)
+}
+/* Get a random stack of N elements with data in range (min, max) */
+func RandomStack(N, min, max int) *Stack {
+    rand.Seed( time.Now().UTC().UnixNano() ) // seed so that you get new stack every time
+    s := new(Stack)
+    for i := 0; i < N; i++ {
+        s.Push(RandIntBetween(min, max))
+    }
+    return s
+}
+
+func (s *Stack) String() string {
+	temporaryStack := new(Stack)
+	var buffer bytes.Buffer
+	for (s.size > 0) {
+		data, _ := s.Pop()
+		temporaryStack.Push(data)
+		buffer.WriteString(fmt.Sprintf("[%v]", data))
+        buffer.WriteString(" -> ")
+	}
+	buffer.WriteString(" BOTTOM ")
+	/* push elements back to the stack*/
+	for (temporaryStack.size > 0) {
+		data, _ := temporaryStack.Pop()
+		s.Push(data)
+	}
+	return buffer.String()
 }
