781c3a94f17a314b497e467d8ce25391bd6a013a
dawsonbotsford <dawsonbotsford@gmail.com>
2015-09-06 00:18:57 -0600

    Completes 1-2

--- javascript/lib/data-structures/chapter-1/1_2.js
+++ javascript/lib/data-structures/chapter-1/1_2.js
@@ -1,12 +1,27 @@
 module.exports = Strings_1_2 = (function() {
   return {
+
+    // NOT inline option
+   //  reverseString: function(str) {
+   //    if (str.length === 0) {
+   //      return '';
+   //    }
+   //    return  str[str.length - 1] + arguments.callee(str.substr(0, str.length - 1));
+   // }
+   //
+   //
+
     /*
-     * Tests to see if string contains all unique chars.
-     * @param {String} str - The string to be checked for uniqueness
-     * @returns {Boolean} true if string has only unique chars. False if a duplicate exists
+     * Reverses a string inline recursively
+     * @param {String} str - The string to be reversed
+     * @returns {String} The reversed string
      */
     reverseString: function(str) {
+      var len = str.length;
+      if (len > 1) {
+        return str[len - 1] + arguments.callee(str.substring(1, len - 1)) + str[0];
+      }
       return str;
-   }
+    }
   };
 }());
--- javascript/test/data-structures/chapter-1/1_2_Spec.js
+++ javascript/test/data-structures/chapter-1/1_2_Spec.js
@@ -3,13 +3,16 @@ describe(Strings_1_2, function () {
   describe('is', function () {
     var str1, str2;
     beforeEach(function () {
-      str1 = "hello";
-      str2 = "olleh";
-      str3 = "abcdefa";
-      str3 = "abcdefa";
+      str1 = 'hello';
+      str2 = 'olleh';
+      str3 = 'abcdef';
+      str4 = 'aedcbb';
     });
     it('returns the reverse of hello', function () {
       expect(Strings_1_2.reverseString(str1)).to.equal(str2);
+    });
+    it('returns the reverse of hello', function () {
+      expect(Strings_1_2.reverseString(str3)).not.to.equal(str4);
     });
   });
 });
