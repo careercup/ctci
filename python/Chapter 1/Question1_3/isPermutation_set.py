aedafb332c166284196c66178433d4cb0e54deb7
unknown <gelarehm@buffalo.edu>
2016-03-25 13:58:39 -0400

    isPermutation implementation for ctci

---
+++ python/Chapter 1/Question1_3/isPermutation_set.py
@@ -0,0 +1,14 @@
+def isPermutation(str1, str2):
+	
+	"""
+	Checks to see if one string is permutation of the other using sets.
+	If statement is necessary because the function will not output correct
+	values for characters that occur more than once otherwise.
+
+	INPUT: Two strings
+	OUTPUT: True or False
+	"""
+        if len(str1) != len(str2):
+            return False
+        else:
+            return set(str1) == set(str2)
